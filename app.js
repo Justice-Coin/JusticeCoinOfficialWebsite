9c3ece8a647b2f9641da53aa0069b1e27d497ea7
Shrey Joshi <shreyjoshi2004@gmail.com>
2020-07-09 15:40:42 -0500

    Add mailchimp subscribing functionality

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
+const https = require('https');
+require('dotenv').config();
 
 app = express();
 
@@ -26,6 +28,47 @@ app.get('/about', function(req, res){
 app.get('/miner', function(req, res){
   res.render('miner', {displaySectionNavs: false});
 });
+
+app.post('/', function(req, res){
+  // Make the mailchimp api and email validation thing into a separate module. Code is getting messy.
+  // Add sophisticated email verificaion and better error messages for mailchimp.
+
+  var emailData = {
+    members: [
+      {
+        email_address: req.body.EMAIL,
+        status: "subscribed",
+      }
+    ]
+  };
+
+  const emailDataJSON = JSON.stringify(emailData);
+  const url = "https://us10.api.mailchimp.com/3.0/lists/" + process.env.MAILCHIMP_LIST_ID;
+  const options = {
+    method: "POST",
+    auth: "JusticeCoin:" + process.env.MAILCHIMP_API_KEY
+  };
+  const request = https.request(url, options, function(response){
+    response.on("data", function(data){
+      if (response.statusCode == 200) {
+        res.render('emailSubscribeSuccess');
+      } else {
+        res.render('emailSubscribeFailure', {statusCode: response.statusCode});
+      }
+    });
+  });
+  request.write(emailDataJSON);
+  request.end();
+});
+
+app.post('/emailSubscribeSuccess', function(req, res){
+  res.redirect('/');
+});
+
+app.post('/emailSubscribeFailure', function(req, res){
+  res.redirect('/#contact');
+});
+
 
 app.listen(3000, function(){
   console.log("Server running on port 3000");
---
+++ mailchimp-config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MAILCHIMP_API_KEY: '33909fada2bb7cab2c2624ee46e276b8-us10',
+  MAILCHIMP_LIST_ID: '25b0459177'
+};
