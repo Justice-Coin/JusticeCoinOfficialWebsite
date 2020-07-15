1ea96c886635ef4c7aafb94014d1ce673a294c3b
Shrey Joshi <shreyjoshi2004@gmail.com>
2020-07-15 13:34:05 -0500

    Added team member icons for social media

--- pageData.js
+++ pageData.js
@@ -19,62 +19,107 @@ module.exports.faqs = [{
 
 module.exports.team = [{
     name: "Pratyush Mallick",
-    position: "Co-Founder & Director of Innovation"
+    position: "Co-Founder & Director of Innovation",
+    instagram: "https://www.instagram.com/pmallick_/",
+    linkedin: "https://www.linkedin.com/in/pratyush-m-993645186/",
+    email: "mailto:",
   },
   {
     name: "Mylinh Murphree",
-    position: "Co-Founder & Director of Operations"
+    position: "Co-Founder & Director of Operations",
+    instagram: "https://www.instagram.com/mylinh_m/",
+    linkedin: "",
+    email: "mailto:mmylinh.murphree@gmail.com",
   },
   {
     name: "Nila Jeyapriya",
-    position: "Director of IT"
+    position: "Director of IT",
+    instagram: "https://www.instagram.com/nila.jeyapriya/",
+    linkedin: "https://www.linkedin.com/in/nilajeyapriya/",
+    email: "mailto:nilajeyapriya@gmail.com",
   },
   {
     name: "Rohit Rajan",
-    position: "Director of Systems Architecture"
+    position: "Director of Systems Architecture",
+    instagram: "https://www.instagram.com/rajansrohit/",
+    linkedin: "https://www.linkedin.com/in/rajansrohit/",
+    email: "mailto:rohitrajan.cs@gmail.com",
   },
   {
     name: "Ethan Davenport",
-    position: "Director of Finance"
+    position: "Director of Finance",
+    instagram: "https://www.instagram.com/ethandav_04/",
+    linkedin: "",
+    email: "mailto:ethandavenport@att.net",
   },
   {
     name: "Yemisi Badmus",
-    position: "Director of Marketing"
+    position: "Director of Marketing",
+    instagram: "https://www.instagram.com/yemisi_o_badmus/",
+    linkedin: "",
+    email: "mailto:",
   },
   {
     name: "Shrey Joshi",
-    position: "Web Development Lead"
+    position: "Web Development Lead",
+    instagram: "https://www.instagram.com/shreyj1729/",
+    linkedin: "https://www.linkedin.com/in/shrey-joshi-005623194/",
+    email: "mailto:",
   },
   {
     name: "Brian Vo",
-    position: "SEO Specialist"
+    position: "SEO Specialist",
+    instagram: "https://www.instagram.com/brianhvo/",
+    linkedin: "https://www.linkedin.com/in/brian-vo-827564189/",
+    email: "mailto:brianhvo0@gmail.com",
   },
   {
     name: "Anay Gupta",
-    position: "Development Lead"
+    position: "Development Lead",
+    instagram: "",
+    linkedin: "https://www.linkedin.com/in/anay-gupta-355028176/",
+    email: "mailto:",
   },
   {
     name: "Cynthia Diep",
-    position: "Social Media Lead"
+    position: "Social Media Lead",
+    instagram: "https://www.instagram.com/cynthiadiep/",
+    linkedin: "http://linkedin.com/in/cynthia-diep-4420351aa",
+    email: "mailto:cynthia.diep1203@gmail.com",
   },
   {
     name: "Ishaan Javali",
-    position: "Developer"
+    position: "Developer",
+    instagram: "https://www.instagram.com/ij_apps/",
+    linkedin: "https://www.linkedin.com/in/ishaan-javali-b2602018b/",
+    email: "mailto:ishaanjav@gmail.com",
   },
   {
     name: "Rohan Ganta",
-    position: "Developer"
+    position: "Developer",
+    instagram: "",
+    linkedin: "",
+    email: "mailto:",
   },
   {
     name: "Sophia Khan",
-    position: "Product Designer"
+    position: "Product Designer",
+    instagram: "https://www.instagram.com/sophiamkhan/",
+    linkedin: "https://www.linkedin.com/in/sophia-m-khan-9b119a1a9/",
+    email: "mailto:smk_1000@hotmail.com",
   },
   {
     name: "Navi Lanka",
-    position: "Marketing Producer"
+    position: "Marketing Producer",
+    instagram: "",
+    linkedin: "",
+    email: "mailto:",
   },
   {
     name: "Tim Murphree",
-    position: "Technical Advisor"
+    position: "Technical Advisor",
+    instagram: "",
+    linkedin: "",
+    email: "mailto:",
   },
 ];
