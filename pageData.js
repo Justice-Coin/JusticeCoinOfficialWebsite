52f16c6e2e3bd9079320a66e9a755707a37552e7
Shrey Joshi <shreyjoshi2004@gmail.com>
2020-07-15 09:21:02 -0500

    put faq and team data in separate module and added .env to gitignore for reference

---
+++ pageData.js
@@ -0,0 +1,80 @@
+//jshint esversion:6
+module.exports.faqs = [{
+    question: "What am I donating when I run JusticeCoin?",
+    answer: "Your computer is being used to verify transactions for the Monero cryptocurrency ledger. This process of reward is known as “mining” and is an incentive to participate in the upkeep of the network. When you run JusticeCoin, you are donating a small percentage of your overall computing power to this task, which results in money we can contribute."
+  },
+  {
+    question: "Why are you mining Monero?",
+    answer: "Monero is an ASIC-resistant cryptocurrency, which means two things: computers designed for the singular purpose of mining cryptocurrency are unable to mine it amd consumer-level computers can mine it in a financially viable way. It also has a stable value relative to other high value cryptocurrencies. The open source code base for Monero mining software made this a tenable project for our team."
+  },
+  {
+    question: "How Much CPU Power Does This Take?",
+    answer: "The browser tab for JusticeCoin uses 10% of your CPU power, which should make it unnoticeable for most users. In the future we will implement a toggle bar allowing you to adjust the computing power."
+  },
+  {
+    question: "Isn't It Faster To Just Donate Cash Directly",
+    answer: "Directly routing money to nonprofits is, of course, the most efficient means of donation. We encourage everyone who has the means to donate directly, in addition to running JusticeCoin. However, this project is also greater than the sum of its parts, or the sum of its hashrate; This is as much about catapulting a radical platform for decentralized financing of non profits as opening the new frontier of non-profit technology. This project seeks to engage people in a dialog about the postive impacts cryptocurrency can have and its implementations for social good."
+  },
+];
+
+module.exports.team = [{
+    name: "Pratyush Mallick",
+    position: "Co-Founder & Director of Innovation"
+  },
+  {
+    name: "Mylinh Murphree",
+    position: "Co-Founder & Director of Operations"
+  },
+  {
+    name: "Nila Jeyapriya",
+    position: "Director of IT"
+  },
+  {
+    name: "Rohit Rajan",
+    position: "Director of Systems Architecture"
+  },
+  {
+    name: "Ethan Davenport",
+    position: "Director of Finance"
+  },
+  {
+    name: "Yemisi Badmus",
+    position: "Director of Marketing"
+  },
+  {
+    name: "Shrey Joshi",
+    position: "Web Development Lead"
+  },
+  {
+    name: "Brian Vo",
+    position: "SEO Specialist"
+  },
+  {
+    name: "Anay Gupta",
+    position: "Development Lead"
+  },
+  {
+    name: "Cynthia Diep",
+    position: "Social Media Lead"
+  },
+  {
+    name: "Ishaan Javali",
+    position: "Developer"
+  },
+  {
+    name: "Rohan Ganta",
+    position: "Developer"
+  },
+  {
+    name: "Sophia Khan",
+    position: "Product Designer"
+  },
+  {
+    name: "Navi Lanka",
+    position: "Marketing Producer"
+  },
+  {
+    name: "Tim Murphree",
+    position: "Technical Advisor"
+  },
+];
