4e9a7adb6461562dee1c8b183e23a6621b548bca
Shrey Joshi <shreyjoshi2004@gmail.com>
2020-08-07 16:01:01 -0500

    make  home page responsive

--- src/Home/Card.jsx
+++ src/Home/Card.jsx
@@ -3,15 +3,16 @@ import {Link as ScrollLink} from "react-scroll";
 
 function Card(props) {
   return (
-    <div className="col-lg-3 col-sm-6" style={{alignItems: "middle"}}>
+    <div className="col-lg-3 col-6 mb-5" style={{alignItems: "middle"}}>
       <ScrollLink className={"Scroll-" + props.route} to={props.route} smooth={true} duration={500}>
-      <button className="card-button mb-5" style={{border: "none"}}>
+      <button className="card-button mb-3" style={{border: "none"}}>
         <div className="card" style={{pointerEvents: "none", backgroundColor: props.bgColor }} >
           <div className="card-body card-link">
             <p className="card-text">{props.text}</p>
           </div>
         </div>
-      </button></ScrollLink>
+      </button>
+      </ScrollLink>
     </div>);
 }
 
